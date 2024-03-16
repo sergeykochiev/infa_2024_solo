089332c59791dfd01d2eef53a1234ca4912de0b0
sergeykochiev <sergeyivanovo547@gmail.com>
2024-03-16 15:45:32 +0300

    port from env

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.enableCors()
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
